2081352c9e205b02f0e7319ed9aa84f270330552
Neil Shevlin <neilshevlin.98@gmail.com>
2022-10-11 18:39:31 +0100

    Remove unneeded packages

--- markdoc/tags/pythonsandbox.markdoc.js
+++
@@ -1,9 +0,0 @@
-import { PythonSandbox } from '../../components/PythonSandbox';
-
-export default {
-  render: PythonSandbox,
-  attributes: {
-    height: { type: String },
-    options: { type: Object }
-  }
-};
